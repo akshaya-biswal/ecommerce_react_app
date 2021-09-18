ce4db7db752be2b73cb9fbf5ac2f3fb762741c25
Akshaya Biswal <akshaya.join@gmail.com>
2021-09-18 23:54:53 +0530

    Custom Slider Added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Announcement from "../components/Announcement";
+
+const Home = () => {
+  return (
+    <>
+      <Announcement />
+    </>
+  );
+};
+
+export default Home;
