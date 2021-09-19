5a4b9e153a2a1907ff02160e5e134f40d6bccd76
Akshaya Biswal <akshaya.join@gmail.com>
2021-09-19 11:58:28 +0530

    Products Component Inprogress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import HeroSlider from "../components/HeroSlider";
 import Categories from "../components/Categories";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar />
       <HeroSlider />
       <Categories />
+      <Products />
     </>
   );
 };
