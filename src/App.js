dafdb297ae42d6ed1dfd680aa6091c5601790ef0
Akshaya Biswal <akshaya.join@gmail.com>
2021-09-19 23:56:19 +0530

    Product Page Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Product from "./pages/Product";
 // import Home from "./pages/Home";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Register /> */}
+      <Product />
     </>
   );
 };
